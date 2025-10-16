03bae631366b2bc675982ab4d37e399593a23b2d
Ezra Akresh <ezraakresh@gmail.com>
2025-10-16 13:31:22 -0400

    Initial Commit

---
+++ Cravr 2048 Demo/Cravr_2048_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cravr_2048_DemoApp.swift
+//  Cravr 2048 Demo
+//
+//  Created by Ezra Akresh on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Cravr_2048_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
